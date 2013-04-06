b7c5a98b6baacde0c8bfe9c9eda2ea3220a299d8
Jeremy Seago <seagoj@gmail.com>
2013-04-06 01:14:01 -0500

    Hides elements on load

--- src/slider.js
+++ src/slider.js
@@ -15,7 +15,7 @@
 
         var $container = this;
         var count = 0;
-        var validTypes = ['Fade','FadeInto','FadeIntoForever'];
+        var validTypes = ['Fade','FadeInto'];
 
         if(validTypes.indexOf(type) !== -1) {
             this["init"+type]();
@@ -34,7 +34,7 @@
      *
      * start    z-index for top-most element in slideshow; Defaults to 100
      */
-    $.fn.initFadeIntoForever = function(start) {
+    $.fn.initFadeInto = function(start) {
         start = typeof start !== 'undefined' ? start : 100
 
         // Adds .active to first element in 'this'
@@ -55,7 +55,7 @@
         }
     };
 
-    $.fn.slideFadeIntoForever = function (intervalID, count, fadeSpeed) {
+    $.fn.slideFadeInto = function (intervalID, count, fadeSpeed) {
         // if(count == 6) clearInterval(intervalID);
         // if(count !== 0 && count%7 == 0 ) this.reset();
 
@@ -93,50 +93,6 @@
                     .fadeIn(fadeSpeed);
             });
         });
-    };
-
-    /**
-     * Sets the initial state for the FadeInto slideshow
-     *
-     * start    z-index for top-most element in slideshow; Defaults to 100
-     */
-    $.fn.initFadeInto = function(start) {
-        start = typeof start !== 'undefined' ? start : 100
-
-        // Adds .active to first element in 'this'
-        this.children(":first").addClass('active');
-
-        // Iterates through all children of 'this'
-        //     sets initiale z-index (start - order in 'this')
-        //     sets position: absolute
-        //     shows child
-        for(var i=1; i<=this.children().length; i=i+1) {
-            this.find(":nth-child("+i+")")
-            .css({
-                'z-index':start-i,
-                'position':'absolute'
-            })
-            .show();
-        }
-    };
-
-    $.fn.slideFadeInto = function (intervalID, count, fadeSpeed) {
-        // if(count == 6) clearInterval(intervalID);
-        // if(count !== 0 && count%7 == 0 ) this.reset();
-
-        fadeSpeed = typeof fadeSpeed !== 'undefined' ? fadeSpeed : 'slow';
-        var $swap = this.swap();
-        var $container = this;
-
-        $swap["active"].fadeOut(fadeSpeed, function() {
-                $swap["active"].hide(function() {
-                   $swap["next"].addClass('active');
-                   var nextZ = $container.nextZ();
-                   $swap["active"].css({'z-index':nextZ}).show();
-                });
-            })
-
-        return count+1;
     };
 
     $.fn.swap = function () {
