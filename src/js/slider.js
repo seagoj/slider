5178ac44c83b155f619c6a89906429f2208758fd
Jeremy Seago <seagoj@gmail.com>
2013-04-08 16:53:14 -0500

    Implementing blur

---
+++ src/js/slider.js
@@ -0,0 +1,106 @@
+(function($) {
+
+    /** 
+     * .slider
+     * 
+     * Begins the slideshow by choosing the type of slide and duration
+     * 
+     * type        Type of slideshow to run; Defaults to 'fade'
+     * duration    Time in miliseconds that each element is visible; Defaults to
+     *                 5000
+     */
+    $.fn.slider = function(options) {
+        var type = typeof options["type"] !== 'undefined' ?
+            options["type"] : 'Fade';
+        var duration = typeof options["duration"] !== 'undefined' ?
+            options["duration"] : 5000;
+
+        var $container = this;
+        var count = 0;
+        var validTypes = ['Fade','FadeInto'];
+
+        if(validTypes.indexOf(type) !== -1) {
+            this["init"+type]();
+
+            var intervalID = setInterval(function() {
+                count = $container["slide"+type](intervalID, count);
+            }, duration);
+        } else {
+            alert(type+" is not a supported slideshow type.");
+            return false;
+        }
+    };
+
+    /**
+     * Sets the initial state for the FadeInto slideshow
+     *
+     * start    z-index for top-most element in slideshow; Defaults to 100
+     */
+    $.fn.initFadeInto = function() {
+        // Adds .active to first element in 'this'
+        this.children(":first").addClass('active');
+
+        // Iterates through all children of 'this'
+        //     sets initiale z-index (start - order in 'this')
+        //     sets position: absolute
+        //     shows child
+        var count = this.children().length;
+        for(var i=1; i<=count; i=i+1) {
+            this.find(":nth-child("+i+")")
+            .css({
+                'z-index':count - i,
+                'position':'absolute'
+            })
+            .show();
+        }
+    };
+
+    $.fn.slideFadeInto = function (intervalID, count, fadeSpeed) {
+        // if(count == 6) clearInterval(intervalID);
+        // if(count !== 0 && count%7 == 0 ) this.reset();
+
+        fadeSpeed = typeof fadeSpeed !== 'undefined' ? fadeSpeed : 'slow';
+        var $swap = this.swap();
+        var $container = this;
+
+        $swap["active"].fadeOut(fadeSpeed, function() {
+            $swap["active"].hide(function() {
+                $swap["next"].addClass('active');
+                var $element = $swap["next"];
+
+                for(var count=$container.children().length;count>0;count=count-1) {
+                    $element.css({'z-index':count}).show();
+
+                    $element = $element.next().length ? $element.next()
+                        : $container.children(":first");
+                }
+            });
+        });
+    };
+
+    $.fn.initFade = function() {
+        this.children().hide().fadeOut();
+        this.children(":first").addClass('active').show();       
+    };
+
+    $.fn.slideFade = function (fadeSpeed) {
+        fadeSpeed = typeof fadeSpeed !== 'undefined' ? fadeSpeed : 'slow';
+        var $swap = this.swap();
+
+        $swap["active"].fadeOut(fadeSpeed, function () {
+            $swap["active"].hide(function () {
+                $swap["next"].addClass('active')
+                    .fadeIn(fadeSpeed);
+            });
+        });
+    };
+
+    $.fn.swap = function () {
+        var $active = this.children(".active").toggleClass('active');
+        var $next = $active.next().length ? $active.next()
+            : this.children(":first");
+                
+        return {"active":$active,"next":$next};
+    };
+
+}) (jQuery);
