ca2127497183718c3bc38264e914b385e46a01ce
Jeremy Seago <seagoj@gmail.com>
2013-04-09 14:17:31 -0500

    Helper functions

---
+++ src/js/devtools.js
@@ -0,0 +1,18 @@
+var merge = function(obj1, obj2) {
+    var ret = obj1;
+
+    for(index in obj2) {
+        ret[index] = obj2[index];    
+    }
+
+    return ret;
+};
+
+var overwrite = function(obj1, obj2) {
+    var ret = obj1;
+
+    for(index in obj2) {
+        if(obj1[index]!=='undefined']
+            obj1[index]=obj2[index];
+    }
+};
